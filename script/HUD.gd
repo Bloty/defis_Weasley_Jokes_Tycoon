cf75bf4d740fb1934b8ae07ca0e62cfa4179946d
LAPTOP-DATK4STS\Nathan <nathan.noyon@gmail.com>
2018-04-19 15:49:28 +0200

    Weasley_Jokes_Tycoon projet godot. score non fonctionnelle

---
+++ script/HUD.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+var score
+
+func _ready():
+	score=0
+
+
+
+
+func up_score(score):
+	score += 1
+
+
+#Cette fonction est appelée Mainchaque fois que le score change.
+func update_score(score):
+	$ScoreLabel.text = str(score)
